e7ca32c1adc2b005d8f436b6064369eb988f8ebd
vikdm <viktorov@my.com>
2022-09-17 17:31:06 +0300

    task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,23 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+
+  const d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d > 0) {
+    let root1 = (-b + Math.sqrt(d)) / (2 * a);
+    let root2 = (-b - Math.sqrt(d)) / (2 * a);
+
+    arr.push(root1);
+    arr.push(root2);
+  } else if ((d === 0)) {
+    let root = -b / (2 * a);
+
+    arr.push(root);
+  }
+
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
