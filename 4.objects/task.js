73e6d58ac7623ebe661a317bf1c31c82598580c8
vikdm <viktorov@my.com>
2022-09-24 18:03:39 +0300

    Объекты. Задание № 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+};
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+};
+
+Student.prototype.addMarks = function (...arrmarks) {
+  for (let i = 0; i < arrmarks.length; i++) {
+    this.addMark(arrmarks[i]);
+  }
 }
 
-// ваш код для остальных методов
+Student.prototype.getAverage = function () {
+  const sum = this.marks.reduce((element, number) => element + number, 0);
+  const sumAverage = sum / this.marks.length;
+  return sumAverage;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.mark;
+
+  this.excluded = reason;
+};
