9a7d8787f47b272350a4f7af4a8d20902265c20b
vikdm <viktorov@my.com>
2022-09-28 21:47:49 +0300

    Классы. Задание № 1 и № 2

---
+++ 5.classes/task.js
@@ -0,0 +1,85 @@
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+
+  fix() {
+    this.state = this.state * 1.5;
+  }
+
+  set state(newStatus) {
+    if (newStatus < 0) {
+      this._state = 0;
+    } else if (newStatus > 100) {
+      this._state = 100;
+    } else {
+      this._state = newStatus;
+    }
+  }
+
+  get state() {
+    return this._state;
+  }
+}
+
+class Magazine extends PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
+  }
+}
+class Book extends PrintEditionItem {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+    this.type = "book";
+  }
+}
+
+class NovelBook extends Book {
+  constructor(name, releaseDate, pagesCount, author) {
+    super(name, releaseDate, pagesCount, author);
+    this.type = "novel";
+  }
+}
+
+class FantasticBook extends Book {
+  constructor(name, releaseDate, pagesCount, author) {
+    super(name, releaseDate, pagesCount, author);
+    this.type = "fantastic";
+  }
+}
+
+class DetectiveBook extends Book {
+  constructor(name, releaseDate, pagesCount, author) {
+    super(name, releaseDate, pagesCount, author);
+    this.type = "detective";
+  }
+}
+
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+    return this.books.find(element => element[type] === value) || null;
+  }
+
+  giveBookByName(bookName) {
+    let result = this.books.find(element => element.name === bookName);
+    this.books.splice(this.books.indexOf(result, 1));
+    return result || null;
+  }
+}
