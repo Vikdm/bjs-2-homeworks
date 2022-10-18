1dfe22d6f26dfd9fcbe678ba82b2e2dc32a89128
vikdm <viktorov@my.com>
2022-10-18 19:13:05 +0300

    Декораторы. Задания № 1 и № 2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,38 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(",");
+    let objectInCache = cache.find((item) => hash === item.hash);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.result);
+      return "Из кэша: " + objectInCache.result;
+    }
+
+    let result = func(...args);
+    cache.push({ hash, result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
+function debounceDecoratorNew(func, delay) {
+  let timeout;
+  let isThrottled = false;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  return function (...args){
+    clearTimeout (timeout);
+    timeout = setTimeout(()=> {
+      func.call(this,...args);
+    }, delay);
+
+    if (!isThrottled){
+      func.call(this,...args);
+      isThrottled = true;
+    }
+  }
 }
